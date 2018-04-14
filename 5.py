8ddf3fe7f92e430145e15d252de86796e2724da4
DavidC117 <38160578+DavidC117@users.noreply.github.com>
2018-04-14 12:32:09 -0500

    IVA

---
+++ 5.py
@@ -0,0 +1,10 @@
+print ("suma de numeros")
+num1 = int (input("dame un numero"))
+num2 = int (input ("dame otro numero"))
+resultado = num1+num2
+print("El resultado de la suma es:",resultado)
+
+cantidad = int (input ("Digita tu cantidad"))
+B = float (input ("dame el iva"))
+print ("El iva es: ", B*cantidad)
+input ("...")
