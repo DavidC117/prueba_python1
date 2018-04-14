c42c618be58b0c602c0c6efbcbfd944d2799f054
DavidC117 <38160578+DavidC117@users.noreply.github.com>
2018-04-14 13:50:51 -0500

    imprimer caracter

---
+++ repetir.py
@@ -0,0 +1,10 @@
+a= input ("escribe:")
+print ("tu palabra es:", a)
+b=int (input ("escribe el caracter a visualizar(numerico)"))
+print ("caracter:",a[b])
+c=int (input("cuantas veces quieres que se imprima"))
+d= a [b]
+res=c*d
+print (res)
+
+input ("...")
