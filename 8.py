1e365f5982608bae0e7800376534a0398ae8f030
DavidC117 <38160578+DavidC117@users.noreply.github.com>
2018-04-14 12:34:10 -0500

    tipo de variable

---
+++ 8.py
@@ -0,0 +1,10 @@
+a=125
+b=3.1416
+print ("tu numero entero es:\n", a,"\ntu numero decimal es:\n",b )
+
+a2=int (b)
+b2=float (a)
+print("tu numero entero es:\n",a,"es un",type(b2))
+print ("tu numero decimal es:\n",b,"es un",type (a2))
+
+input (...)
