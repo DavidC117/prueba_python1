ff274bf42c45dc93582491b27ecc5ee3a90cb53c
DavidC117 <38160578+DavidC117@users.noreply.github.com>
2018-04-14 13:50:04 -0500

    Add files via upload

---
+++ maysuculas a minusculas y listas.py
@@ -0,0 +1,26 @@
+a = "ESTO SE HARA EN MINISCULAS"
+b= a.lower()
+print( a,b )
+
+input ("...")
+
+a= "David"
+b= "masculino"
+lista=[a,"Castillo",1.71,312212522,b]
+tamaño=len(lista)
+tipo=type (lista)
+print (lista)
+print (tamaño)
+print (tipo)
+lista.append (80)
+print (lista)
+d=lista.index(b)
+print (d)
+lista.extend ("hola")
+print (lista)
+lista.insert (2,99)
+print (lista)
+lista.pop(0)
+print (lista)
+
+input ("...")
